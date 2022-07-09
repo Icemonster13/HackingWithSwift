c9f88ba87079044a6baf13a1827888324d2054b0
Michael Pascucci <mdpascucci@live.com>
2022-07-09 17:06:34 -0400

    Add Project 1 - WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michael & Diana Pascucci on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
