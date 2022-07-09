d0e26c078a581120cd797347049b5455ebdf8d6f
Michael Pascucci <mdpascucci@live.com>
2022-07-09 17:15:39 -0400

    Add WeSplit Project

---
+++ Project-1-WeSplit/Project-1-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-1-WeSplit
+//
+//  Created by Michael & Diana Pascucci on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-1-WeSplit/Project-1-WeSplit/Project_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_WeSplitApp.swift
+//  Project-1-WeSplit
+//
+//  Created by Michael & Diana Pascucci on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
