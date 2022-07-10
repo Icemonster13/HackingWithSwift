7dd04157406b171419dd99ae6b4ef8152f9775cb
Michael Pascucci <mdpascucci@live.com>
2022-07-09 20:54:00 -0400

    Add Challenge-Day 19 Project

---
+++ Challenge-Day19/Challenge-Day19/ChallengeDay19App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_Day19App.swift
+//  Challenge-Day19
+//
+//  Created by Michael & Diana Pascucci on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
